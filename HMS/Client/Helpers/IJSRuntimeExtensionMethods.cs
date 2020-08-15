a32e3ac43ad99e674e6ab5bbcf1054f957039e2a
simbert1 <69716955+simbert1@users.noreply.github.com>
2020-08-15 15:10:14 +0200

    Initial commit

---
+++ HMS/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMS.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime jS ,string message)
+        {
+            return await jS.InvokeAsync<bool>("confirm",message);
+        }
+    }
+}
